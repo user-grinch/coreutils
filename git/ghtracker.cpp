e0237b64cfc81364cf0e115fb68f30a19cf576f2
Grinch_ <user.grinch@gmail.com>
2025-10-01 13:38:50 +0600

    Remove duplicates

--- git/ghtracker.cpp
+++ git/ghtracker.cpp
@@ -1,84 +1,80 @@
-#include <Urlmon.h>
-
 #include "ghtracker.h"
-#include "storage/json.hpp"
-
+#include <Urlmon.h>
 #include <fstream>
+#include <sstream>
 
-GHTracker::GHTracker(const std::string &userName, const std::string &repoName, const std::string &currentVer)
+GHTracker::GHTracker(const std::string& userName, const std::string& repoName, const std::string& currentVer)
     : localVer(currentVer)
 {
     link = "https://api.github.com/repos/" + userName + "/" + repoName + "/tags";
 }
 
-void GHTracker::CheckUpdate(bool showMSG)
-{
+void GHTracker::CheckUpdate(bool showMSG) {
     curState = eStates::Checking;
     showMessage = showMSG;
 }
 
-std::string GHTracker::GetUpdateVersion() const
-{
+std::string GHTracker::GetUpdateVersion() const {
     return latestVer;
 }
 
-bool GHTracker::IsUpdateAvailable() const
-{
+bool GHTracker::IsUpdateAvailable() const {
     return curState == eStates::Found;
 }
 
-void GHTracker::ResetUpdaterState()
-{
+void GHTracker::ResetUpdaterState() {
     curState = eStates::Idle;
 }
 
-static bool IsVersionNewer(const std::string &latest, const std::string &current)
-{
-    return latest > current;
+static bool IsVersionNewer(const std::string& latest, const std::string& current) {
+    return latest > current; // Replace with semantic version comparison if needed
 }
 
-void GHTracker::Process(std::function<void(eStates, bool)> eventCallback, const std::string &path, bool lockDowngrade)
-{
-    if (curState != eStates::Checking)
-        return;
+// --- Minimal JSON "name" extractor ---
+static std::string ExtractFirstTagName(const std::string& jsonText) {
+    const std::string key = "\"name\"";
+    size_t pos = jsonText.find(key);
+    if (pos == std::string::npos) return {};
+
+    // Find the first quote after "name":
+    pos = jsonText.find('"', pos + key.size());
+    if (pos == std::string::npos) return {};
+    size_t start = pos + 1;
+
+    // Find the closing quote
+    size_t end = jsonText.find('"', start);
+    if (end == std::string::npos) return {};
+
+    return jsonText.substr(start, end - start);
+}
+
+void GHTracker::Process(std::function<void(eStates, bool)> eventCallback, const std::string& path, bool lockDowngrade) {
+    if (curState != eStates::Checking) return;
 
     curState = eStates::Failed;
 
-    if (URLDownloadToFile(nullptr, link.c_str(), path.c_str(), 0, nullptr) != S_OK)
-    {
+    if (URLDownloadToFile(nullptr, link.c_str(), path.c_str(), 0, nullptr) != S_OK) {
         eventCallback(curState, showMessage);
         return;
     }
 
     std::ifstream file(path);
-    if (!file.is_open())
-    {
+    if (!file.is_open()) {
         eventCallback(curState, showMessage);
         return;
     }
 
-    nlohmann::json data;
-    try
-    {
-        data = nlohmann::json::parse(file);
-    }
-    catch (...)
-    {
-        file.close();
-        eventCallback(curState, showMessage);
-        return;
-    }
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
     file.close();
 
-    if (data.empty() || !data[0].contains("name"))
-    {
-        curState = eStates::NotFound;
-    }
-    else
-    {
-        latestVer = data[0]["name"].get<std::string>();
-        bool isNewer = IsVersionNewer(latestVer, localVer);
+    std::string jsonText = buffer.str();
+    latestVer = ExtractFirstTagName(jsonText);
 
+    if (latestVer.empty()) {
+        curState = eStates::NotFound;
+    } else {
+        bool isNewer = IsVersionNewer(latestVer, localVer);
         curState = (isNewer || !lockDowngrade) ? eStates::Found : eStates::NotFound;
     }
 
