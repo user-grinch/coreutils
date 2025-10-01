c3cd64ddf48caffa7fe3dd5f23c1e0570872ac56
Grinch_ <user.grinch@gmail.com>
2025-10-01 13:51:27 +0600

    add matmgr, vkeys etc.

--- online/ghtracker.h
+++ online/ghtracker.h
@@ -1,9 +1,8 @@
 #pragma once
-#include <functional>
 #include <string>
+#include <functional>
 
-enum class eStates
-{
+enum class eStates {
     Idle,
     Checking,
     Found,
@@ -11,20 +10,19 @@ enum class eStates
     Failed,
 };
 
-class GHTracker
-{
-  private:
+class GHTracker {
+private:
     eStates curState = eStates::Idle;
     std::string latestVer;
     std::string link;
     std::string localVer;
     bool showMessage = false;
 
-  public:
-    GHTracker(const std::string &userName, const std::string &repoName, const std::string &currentVer);
+public:
+    GHTracker(const std::string& userName, const std::string& repoName, const std::string& currentVer);
 
     void CheckUpdate(bool showMSG = false);
-    void Process(std::function<void(eStates, bool)> eventCallback, const std::string &path, bool lockDowngrade = true);
+    void Process(std::function<void(eStates, bool)> eventCallback, const std::string& path, bool lockDowngrade = true);
 
     [[nodiscard]] std::string GetUpdateVersion() const;
     [[nodiscard]] bool IsUpdateAvailable() const;
