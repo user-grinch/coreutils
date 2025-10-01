b992ec141326a4f99f5728392626c1b5ae088561
Grinch_ <user.grinch@gmail.com>
2025-10-01 13:25:02 +0600

    update ghtracker

--- online/ghtracker.cpp
+++ online/ghtracker.cpp
@@ -1,46 +1,82 @@
 #include "ghtracker.h"
+#include <Urlmon.h>
 #include <fstream>
 #include <sstream>
-#include <Urlmon.h>
-#include "storage/json.hpp"
-#include "storage/registry.h"
 
-extern RegistryWrapper gRegistry;
+GHTracker::GHTracker(const std::string& userName, const std::string& repoName, const std::string& currentVer)
+    : localVer(currentVer)
+{
+    link = "https://api.github.com/repos/" + userName + "/" + repoName + "/tags";
+}
 
-void GHTracker::Process(std::function<void(eStates, bool)> eventCallback, std::string path, bool lockDowngrade) {
-    if (curState != eStates::Checking) {
-        return;
-    }
+void GHTracker::CheckUpdate(bool showMSG) {
+    curState = eStates::Checking;
+    showMessage = showMSG;
+}
+
+std::string GHTracker::GetUpdateVersion() const {
+    return latestVer;
+}
+
+bool GHTracker::IsUpdateAvailable() const {
+    return curState == eStates::Found;
+}
+
+void GHTracker::ResetUpdaterState() {
+    curState = eStates::Idle;
+}
+
+static bool IsVersionNewer(const std::string& latest, const std::string& current) {
+    return latest > current; // Replace with semantic version comparison if needed
+}
+
+// --- Minimal JSON "name" extractor ---
+static std::string ExtractFirstTagName(const std::string& jsonText) {
+    const std::string key = "\"name\"";
+    size_t pos = jsonText.find(key);
+    if (pos == std::string::npos) return {};
+
+    // Find the first quote after "name":
+    pos = jsonText.find('"', pos + key.size());
+    if (pos == std::string::npos) return {};
+    size_t start = pos + 1;
+
+    // Find the closing quote
+    size_t end = jsonText.find('"', start);
+    if (end == std::string::npos) return {};
+
+    return jsonText.substr(start, end - start);
+}
+
+void GHTracker::Process(std::function<void(eStates, bool)> eventCallback, const std::string& path, bool lockDowngrade) {
+    if (curState != eStates::Checking) return;
+
     curState = eStates::Failed;
 
-    HRESULT res = URLDownloadToFile(nullptr, link.c_str(), path.c_str(), 0, nullptr);
-    if (res == S_OK) {
-        std::ifstream file(path);
-        if (!file.is_open()) {
-            curState = eStates::Failed;
-            return;
-        }
-        nlohmann::json data = nlohmann::json::parse(file);
-        file.close();
-
-        if (!data.empty()) {
-            latestVer = data[0]["name"];
-            if (lockDowngrade) {
-                std::string regVer = std::to_string(gRegistry.GetValue(REGISTRY_KEY));
-                if (regVer == "" | latestVer > localVer || regVer > localVer) {
-                    curState = eStates::Found;
-                    gRegistry.SetValue(REGISTRY_KEY, std::stoi(GetUpdateVersion()));
-                } else {
-                    curState = eStates::NotFound;
-                }
-            } else {
-                if (latestVer > localVer) {
-                    curState = eStates::Found;
-                } else {
-                    curState = eStates::NotFound;
-                }
-            }
-        }
+    if (URLDownloadToFile(nullptr, link.c_str(), path.c_str(), 0, nullptr) != S_OK) {
+        eventCallback(curState, showMessage);
+        return;
     }
+
+    std::ifstream file(path);
+    if (!file.is_open()) {
+        eventCallback(curState, showMessage);
+        return;
+    }
+
+    std::ostringstream buffer;
+    buffer << file.rdbuf();
+    file.close();
+
+    std::string jsonText = buffer.str();
+    latestVer = ExtractFirstTagName(jsonText);
+
+    if (latestVer.empty()) {
+        curState = eStates::NotFound;
+    } else {
+        bool isNewer = IsVersionNewer(latestVer, localVer);
+        curState = (isNewer || !lockDowngrade) ? eStates::Found : eStates::NotFound;
+    }
+
     eventCallback(curState, showMessage);
 }
--- online/ghtracker.h
+++ online/ghtracker.h
@@ -13,31 +13,18 @@ enum class eStates {
 class GHTracker {
 private:
     eStates curState = eStates::Idle;
-    std::string latestVer, link, localVer;
+    std::string latestVer;
+    std::string link;
+    std::string localVer;
     bool showMessage = false;
 
 public:
-    GHTracker(const std::string& userName, const std::string & repoName, const std::string& currentVer) {
-        link = "https://api.github.com/repos/" + userName + "/" + repoName + "/tags";
-        localVer = currentVer;
-    }
+    GHTracker(const std::string& userName, const std::string& repoName, const std::string& currentVer);
 
-    void CheckUpdate(bool showMSG = false) {
-        curState = eStates::Checking;
-        showMessage = showMSG;
-    }
+    void CheckUpdate(bool showMSG = false);
+    void Process(std::function<void(eStates, bool)> eventCallback, const std::string& path, bool lockDowngrade = true);
 
-    _NODISCARD std::string GetUpdateVersion() {
-        return latestVer;
-    }
-
-    _NODISCARD bool IsUpdateAvailable() {
-        return curState == eStates::Found;
-    }
-
-    void ResetUpdaterState() {
-        curState = eStates::Idle;
-    }
-
-    void Process(std::function<void(eStates, bool)> eventCallback, std::string path, bool lockDowngrade = true);
+    [[nodiscard]] std::string GetUpdateVersion() const;
+    [[nodiscard]] bool IsUpdateAvailable() const;
+    void ResetUpdaterState();
 };
